c27bcc90d4ddd3f70ee7db234b215decb9f71d61
htb2767 <buithihuong2710@gmail.com>
2023-07-16 18:44:46 -0700

    customer and vendor registration

--- accounts/models.py
+++ accounts/models.py
@@ -62,6 +62,12 @@ class User(AbstractBaseUser):
         return self.is_admin
     def has_module_perms(self,app_label):
         return True
+    def get_role(self):
+        if self.role==1:
+            user_role='Vendor'
+        elif self.role==2:
+            user_role='Customer'
+            return user_role
     
 class UserProfile(models.Model):
     user = OneToOneField(User,on_delete=models.CASCADE,blank=True,null=True)
@@ -79,3 +85,4 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.email
+    
