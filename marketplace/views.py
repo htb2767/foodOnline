299b7a3ca3f53ceeaa91ab2a91c269c4ef240a8b
htb2767 <buithihuong2710@gmail.com>
2023-08-24 18:38:05 -0700

    business opening hour

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,6 +1,7 @@
+from datetime import date,datetime
 from django.shortcuts import render,get_object_or_404,redirect
 from django.http import JsonResponse
-from vendor.models import Vendor
+from vendor.models import OpeningHour, Vendor
 from menu.models import Category,FoodItem
 from django.db.models import Prefetch
 from .models import Cart
@@ -28,6 +29,14 @@ def vendor_detail(request,vendor_slug):
         queryset=FoodItem.objects.filter(is_available=True)
         )
     )
+    opening_hours=OpeningHour.objects.filter(vendor=vendor).order_by('day','-from_hour')
+    #check current state for opening hour
+    today_date=date.today()
+    today=today_date.isoweekday()
+    current_opening_hours=OpeningHour.objects.filter(vendor=vendor,day=today)
+    
+
+
     if request.user.is_authenticated:
         cart_items=Cart.objects.filter(user=request.user)
     else:
@@ -37,6 +46,9 @@ def vendor_detail(request,vendor_slug):
         'vendor':vendor,
         'categories':categories,
         'cart_items':cart_items,
+        'opening_hours':opening_hours,
+        'current_opening_hours':current_opening_hours,
+        
     }
     return render(request,'marketplace/vendor_detail.html',context)
 def add_to_cart(request,food_id=None):
