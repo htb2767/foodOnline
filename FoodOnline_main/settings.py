abc26746ca0ef225975d19fae0dd5864ccf6d2f1
htb2767 <buithihuong2710@gmail.com>
2023-10-02 08:13:01 -0700

    new changes to go live

--- FoodOnline_main/settings.py
+++ FoodOnline_main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.233.197.53','127.0.0.1','huongfood.online','www.huongfood.online']
 
 
 # Application definition
